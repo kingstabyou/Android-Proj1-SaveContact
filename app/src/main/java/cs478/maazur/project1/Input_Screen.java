6a0613880a29bd26a20d2c3faab1748bedef9ad5
maaz <rahman.maazur@gmail.com>
2021-01-31 02:30:51 -0600

    Messaging between act1 and 2 in place

--- app/src/main/java/cs478/maazur/project1/Input_Screen.java
+++ app/src/main/java/cs478/maazur/project1/Input_Screen.java
@@ -4,25 +4,32 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class Input_Screen extends AppCompatActivity {
- public static final String EXTRA_MESSAGE="name";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input__screen);
-    }
-
-
-
-    public void Full_Name(View view){
-        EditText name = (EditText) findViewById(R.id.Input_Fullname);
-        String fullname= name.getText().toString();
-        Intent intent = new Intent(this,MainActivity.class);
-        intent.putExtra(EXTRA_MESSAGE,fullname);
-        setResult(RESULT_OK,intent);
-        finish();
+        Button button1 =(Button)findViewById(R.id.button);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText name = (EditText) findViewById(R.id.Input_Fullname);
+                String fullname= name.getText().toString().trim();
+                Log.i("ter",fullname);
+                Intent intent= new Intent(getApplicationContext(),MainActivity.class);
+                intent.putExtra("Message",fullname);
+                if(1==1) {
+                    setResult(RESULT_OK, intent);
+                }else {
+                    setResult(RESULT_CANCELED, intent);
+                }
+                finish();
+            }
+        });
     }
 }
